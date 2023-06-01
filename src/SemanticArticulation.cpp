8255a5a8b463421f4a03a094884120ea1140909c
Frithjof Vollmer <122092828+FrithjofVollmer@users.noreply.github.com>
2023-06-01 12:23:02 +0200

    Activate overlap compensation by default in SemanticArticulation.cpp

--- src/SemanticArticulation.cpp
+++ src/SemanticArticulation.cpp
@@ -185,7 +185,7 @@ SemanticArticulation::SemanticArticulation(float inputSampleRate) :
          { "noteDurations", 1.f },
          { "reverb", 1.f },
          { "soundQuality", 2.f },
-         { "overlapCompensationFactor", 1.6f }
+         { "overlapCompensationFactor", on }
      })
     {}
     
