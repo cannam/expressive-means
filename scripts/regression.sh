829158e8d15cb72612021149b854d449eb59a5e6
Chris Cannam <cannam@all-day-breakfast.com>
2023-06-27 14:34:44 +0100

    Regression

---
+++ scripts/regression.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Requires a known-quantity input file. We use
+#   "test-material/1953 Szeryng_Beethoven op. 61, 2nd mov, 43-44.wav"
+#   (1456338 bytes, SHA-1 fbc7a2b23d332debea00143e07fc797ac5fea98d)
+
+set -eu
+
+input="test-material/1953 Szeryng_Beethoven op. 61, 2nd mov, 43-44.wav"
+
+if [ ! -f "$input" ]; then
+    echo "Input file $input not found"
+    exit 2
+fi
+
+while read output ; do 
+    echo "--- Testing $output"
+    VAMP_PATH=./build vamp-simple-host "expressive-means:$output" "$input" > /tmp/$$
+    expected=scripts/regression-expected/"$(echo $output | sed s/:/_/g)"
+    if diff -u /tmp/$$ "$expected" ; then
+        echo "--- Passed"
+    else
+        echo "*** FAILED"
+        exit 1
+    fi
+    echo
+done <<EOF
+    onsets:onsets
+    articulation:summary
+    pitch-vibrato:summary
+    portamento:summary
+EOF
+
